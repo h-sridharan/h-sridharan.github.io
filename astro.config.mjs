c77f89b49f685deb6f2a72870bc3099c96edcce1
curtis-murray <murrcurt09@gmail.com>
2024-04-17 17:30:00 +1000

    change address

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'haripriyasridharan.github.io',
+  site: 'h-sridharan.github.io',
   integrations: [tailwind(), react()],
   vite: {
     ssr: {
